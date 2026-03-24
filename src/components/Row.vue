2a0f8f8a2782be24acdcfd4c18b65ef8159082a1
kk <krausko@outlook.de>
2026-03-24 16:07:16 +0100

    feat: project initialization

---
+++ src/components/Row.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <v-row>
+    <v-col>1</v-col>
+    <v-col>1</v-col>
+    <v-col>1</v-col>
+  </v-row>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Sudoku.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+import Section from "./Section.vue";
+</script>
+
+<template>
+  <h1>Hello World</h1>
+
+  <v-container>
+    <v-row>
+      <v-col>
+        <Section/>
+      </v-col>
+    </v-row>
+
+  </v-container>
+
+</template>
+
+<style scoped>
+table {
+  border: 2px solid black;
+}
+
+td {
+  border: 1px solid black;
+}
+
+.td-side {
+  border-right: 2px solid black;
+}
+
+.td-bottom {
+  border-bottom: 2px solid black;
+}
+
+div {
+  width: 100%;
+}
+</style>
