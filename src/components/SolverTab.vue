c539f76fe8df0bea8d478f5d2d119d7cbcfeca7a
kk <krausko@outlook.de>
2026-08-20 14:06:48 +0200

    fix: corrected solver selection.

--- src/components/SolverTab.vue
+++ src/components/SolverTab.vue
@@ -1,24 +1,23 @@
 <script lang="ts" setup>
 
-// TODO möglichkeit solver und option auszuwählen hinzufügen
-
 import {SolverManager, solvers} from "./SolverManager.ts";
 import type {Sudoku} from "../types/Sudoku.ts";
 import type {SolverEvent} from "../types/SolverEvent.ts";
+import type {Solver} from "../types/Solver.ts";
 
 const props = defineProps<{
   sudoku: Sudoku;
 }>();
 
-let manager: SolverManager = new SolverManager();
-
-manager.setSolver(solvers[0])
+const manager: SolverManager = new SolverManager();
+let selectedSolver: Solver = solvers[0];
 
 const emit = defineEmits<{
   (event: 'solverEvents', value: SolverEvent[]): void
 }>()
 
 const start = () => {
+  manager.setSolver(selectedSolver)
   manager.setBoard(props.sudoku);
   emit('solverEvents', manager.solveSteps());
 }
@@ -26,8 +25,18 @@ const start = () => {
 </script>
 
 <template>
-  <p>solvert-tab</p>
-  <button @click="start">Start Solver</button>/
+  <p>select algorithm</p>
+  <select v-model="selectedSolver">
+    <option
+        v-for="solver in solvers"
+        :key="solver.name"
+        :value="solver"
+    >
+      {{ solver.name }}
+    </option>
+  </select>
+
+  <button @click="start">Start Solver</button>
 </template>
 
 <style scoped>
